1c6cf51ef98e029155c30171afb89e3aee167cbe
Ayberk Yiğit <kurtknispel9@gmail.com>
2023-02-11 13:04:41 +0300

    first commit

---
+++ src/components/layout/layout.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import { IntlProvider,FormattedMessage } from "react-intl";
+import LayoutContext from "./layoutContext";
+
+
+const message = {
+  en:{
+    heading:"Heading",
+    heading_main_menu:"Demo Main Menu",
+    title:"Title",
+    footer:"Footer",
+  },
+  tr:{
+    heading:"Başlık",
+    heading_main_menu:"Demo Anasayfa",
+    title:"Başlık",
+    footer:"Alt kısım"
+  }
+}
+
+
+//Using Redux reducers at the top of the component tree
+//import { useSelector, useDispatch } from "react-redux";
+//import {increment,decrement,incrementByAmount} from '../../redux/counter'
+
+//Might be useful for later
+//import { useOnScreen,useMediaQuery } from '../../utils/hooks';
+//
+
+
+export default function Layout({ children }) {
+    //Get the global state
+   // const count = useSelector(state => state.counter.value)
+   // const dispatch = useDispatch()
+    //using dispatch function to dispatch actions
+    
+
+
+ // const [size, setSize] = useState();
+  //const [ref, visible] = useOnScreen({ rootMargin: '-100px' })
+  //const size = useMediaQuery(768)
+  // useEffect(() => {
+  //   setSize(document.body.clientWidth);
+  // }, []);
+  // useEffect(() => {
+  //   const onResize = (e) => {
+  //     setSize(e.target.outerWidth);
+  //   };
+  //   window.addEventListener("resize", onResize);
+  //   return () => {
+  //     window.removeEventListener("resize", onResize);
+  //   };
+  // }, []);
+
+  const [locale, setLocale] = useState('en');
+
+  const handleChange = (e) => {
+    setLocale(e.target.value);
+  };
+
+  return (
+    <>
+    <LayoutContext.Provider value={{locale}} >
+    <select onChange={handleChange}>
+      {['en','tr'].map((x)=>(
+        <option value={x} key={x}>{x}</option>
+      ))}
+    </select>
+    <IntlProvider locale={locale} messages={message[locale]} >
+    <Header title={<FormattedMessage id="heading" defaultMessage="Default" values={{locale}} />} />
+     {/* Example for how to use @reduxjs/toolkit */}
+     {/* <button onClick={()=> dispatch(increment())} >Increment</button>
+     <button onClick={()=> dispatch(decrement())} >Decrement</button>
+     <button onClick={()=> dispatch(incrementByAmount(5))} >Increment by 5</button>
+     {count}  */}
+     <main title="Main">
+       {children}
+     </main>
+     <Footer footer_title={<FormattedMessage id="heading" defaultMessage="Default" values={{locale}} />} />
+    </IntlProvider>
+    </LayoutContext.Provider>
+    </>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,44 @@
-import '@/styles/globals.css'
+import App from "next/app";
+import Router from "next/router";
+import Layout from "../components/layout/layout";
+let copies = [];
+const onLoad = () => {
+  const nodes = document.querySelectorAll(
+    "link[rel=stylesheet], style:not([media=x])"
+  );
+  copies = [...nodes].map((el) => el.cloneNode(true));
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  for (let copy of copies) {
+    copy.removeAttribute("data-n-p");
+    copy.removeAttribute("data-n-href");
+
+    document.head.appendChild(copy);
+  }
+};
+
+const onExit = () => {
+  for (let copy of copies) {
+    document.head.removeChild(copy);
+  }
+};
+class MyApp extends App {
+  componentDidMount() {
+    Router.events.on("beforeHistoryChange", onLoad);
+  }
+  componentWillUnmount() {
+    Router.events.off("beforeHistoryChange", onExit);
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+    return (
+      <>
+        <Layout>
+              <Component {...pageProps} key={router.route} />
+        </Layout>
+      </>
+    );
+  }
 }
+
+export default MyApp;
